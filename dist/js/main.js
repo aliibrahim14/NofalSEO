8ee231872a90af67e4a8688248d8916de090cc2a
aliibrahim14 <ali_g7@yahoo.com>
2021-06-09 17:25:57 +0200

    Finish Home Page

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,6 +2,31 @@
 $(function () {
     "use strict";
     $(document).ready(function () {
+        $('.owl-carousel.customers').owlCarousel({
+            rtl: true,
+            loop: true,
+            margin: 30,
+            autoplay: false,
+            autoplayTimeout: 2000,
+            autoplayHoverPause: true,
+            navText: ['<i class="fas fa-chevron-right"></i>', '<i class="fas fa-chevron-left"></i>'],
+            responsive: {
+                0: {
+                    items: 1
+                },
+                500: {
+                    items: 2
+                },
+                780: {
+                    items: 3
+                },
+                992: {
+                    items: 4
+                }
+            }
+        });
+    });
+    $(document).ready(function () {
         $('.owl-carousel').owlCarousel({
             rtl: true,
             loop: true,
